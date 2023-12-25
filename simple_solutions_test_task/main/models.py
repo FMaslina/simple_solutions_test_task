737652112090b969a3cfaae90e64936231040f1a
FMaslina <FMaslina@yandex.ru>
2023-12-25 20:49:32 +0700

    Start app "main", create model "Item"

---
+++ simple_solutions_test_task/main/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.TextField(verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=10)
