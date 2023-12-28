65b807799ed01c8369e53e0a94540f5fc72ad560
FMaslina <FMaslina@yandex.ru>
2023-12-28 16:27:10 +0700

    Add "item/get/id" urls

--- simple_solutions_test_task/main/urls.py
+++ simple_solutions_test_task/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GetStripeSessionIdView, CreateItem
+from .views import GetStripeSessionIdView, CreateItem, GetItem
 
 urlpatterns = [
     path('buy/<int:id>/', GetStripeSessionIdView.as_view(), name='buy-detail'),
-    path('item/create', CreateItem.as_view(), name='item-create')
+    path('item/create', CreateItem.as_view(), name='item-create'),
+    path('item/get/<int:id>', GetItem.as_view(), name='item-get')
 ]
