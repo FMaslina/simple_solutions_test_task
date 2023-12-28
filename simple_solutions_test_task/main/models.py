d618910c4b4ad89296002e1043e9f246db7cca3a
FMaslina <FMaslina@yandex.ru>
2023-12-28 13:51:57 +0700

    Update Item model

--- simple_solutions_test_task/main/models.py
+++ simple_solutions_test_task/main/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Item(models.Model):
     name = models.TextField(verbose_name="Название")
     description = models.TextField(verbose_name="Описание")
-    price = models.DecimalField(max_digits=10, decimal_places=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
